4e40cb31cd517979f79d035b3632f007f1df8c2e
zknight666 <55696400+zknight666@users.noreply.github.com>
2023-01-08 22:42:29 +0900

    2023.01.08

--- keras/keras16_validation7_diabets.py
+++ keras/keras16_validation7_diabets.py
@@ -5,88 +5,83 @@ from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-#1. data
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# 1. data
 
 
-dataset=load_diabetes()
-x=dataset.data
-y=dataset.target
+dataset = load_diabetes()
+x = dataset.data
+y = dataset.target
 
-print(x.shape) # (442, 10)
+print(x.shape)  # (442, 10)
 print(y.shape)
 
 
-x_train,x_test,y_train,y_test=train_test_split(
-    x,y,
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y,
     train_size=0.8,
-    random_state=1,
+    random_state=78,
     shuffle=True
 )
 
 
-#2. model
-model=Sequential()
-model.add(Dense(100,input_dim=10,activation='relu'))
-model.add(Dense(500,activation='relu'))
+# 2. model
+model = Sequential()
+model.add(Dense(5, input_dim=10, activation='relu'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(5, activation='relu'))
 # model.add(Dense(500,activation='relu'))
 # model.add(Dense(100))
 # model.add(Dense(500))
 # model.add(Dense(100))
 # model.add(Dense(64,activation='relu'))
 model.add(Dense(1))
-          
-          
-          
-          
 
-    
-    
-#3. compile, training
+
+# 3. compile, training
 model.compile(
     optimizer='adam',
     loss='mae',
-    metrics=['accuracy']
+    metrics=['mse']
 )
+
+Early_stopping = EarlyStopping(
+    monitor='val_loss', patience=4, verbose=2, restore_best_weights=True)
+
 
 model.fit(
-    x_train,y_train,
+    x_train, y_train,
     batch_size=1,
-    epochs=500,
-    validation_split=0.2
+    epochs=1500,
+    validation_split=0.2,
+    callbacks=[Early_stopping]
 )
 
 
-#4. 평가 ,예측
+# 4. 평가 ,예측
 
 
-loss=model.evaluate(x_test,y_test)
+loss = model.evaluate(x_test, y_test)
 
-y_predict=model.predict(x_test)
-
-
-def RMSE(y_test,y_predict):
-    return(np.sqrt(mean_squared_error(y_test,y_predict)))
+y_predict = model.predict(x_test)
 
 
+def RMSE(y_test, y_predict):
+    return(np.sqrt(mean_squared_error(y_test, y_predict)))
 
 
-
-print('loss',loss)
-print('RMSE',RMSE(y_test,y_predict))
-print('r2',r2_score(y_test,y_predict))
+print('loss', loss)
+print('RMSE', RMSE(y_test, y_predict))
+print('r2', r2_score(y_test, y_predict))
 
 # r2 값 0.62 이상
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+결과
+loss [39.077388763427734, 39.077388763427734]
+RMSE 47.94034590318054
+r2 0.6345808155342813
+"""
--- keras/keras16_validation8_dacon_ddarung.py
+++ keras/keras16_validation8_dacon_ddarung.py
@@ -1,32 +1,105 @@
 import numpy as np
+import pandas as pd
+import time
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
-import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
-
-#1. data
+# 1. data (data 불러오기, column 확인, 결측치 확인, 데이터 전처리)
 """
 pd.read_csv
 """
 
-path='./_data/ddarung/'
-train_csv=pd.read_csv(path+'train.csv',index_col=[0,9])
-test_csv=pd.read_csv(path+'test.csv',index_col=0)
-submission_csv=pd.read_csv(path+'submission.csv',index_col=0)
+path = './_data/ddarung/'
+
+# [1459 rows x 10 columns]
+train_csv = pd.read_csv(path+'train.csv', index_col=[0])
+test_csv = pd.read_csv(path+'test.csv', index_col=0)  # [715 rows x 9 columns]
+submission_csv = pd.read_csv(path+'submission.csv', index_col=0)
 
 print(train_csv)
 print(test_csv)
 print(submission_csv)
 
-print(train_csv.shape) # (1459, 9)
-print(test_csv.shape) # (715, 9)
-print(submission_csv.shape) # (715, 1)
+print(train_csv.shape)  # (1459, 10)
+print(test_csv.shape)  # (715, 9)
+print(submission_csv.shape)  # (715, 1)
 
-print(train_csv.columns) # column 확인
+print(train_csv.columns)  # column 확인
+
+print(train_csv.info())  # 결측치 확인
+print(test_csv.info())
+print(train_csv.describe())  # 최대,최소값 확인
 
 
+print(train_csv.isnull().sum())
+train_csv = train_csv.dropna()  # 결측치 삭제
+
+x = train_csv.drop('count', axis=1)
+y = train_csv['count']
+print(x)
+print(y)
 
 
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y,
+    train_size=0.8,
+    random_state=1
+)
 
+print(x_train.shape, x_test.shape)  # (1167, 9) (292, 9)
+print(y_train.shape, y_test.shape)  # (1167,) (292,)
+
+
+# 2. model
+model = Sequential()
+model.add(Dense(50, input_dim=9))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(1))
+
+# 3. compile, training
+
+model.compile(
+    optimizer='nadam',
+    loss='mae',
+    metrics=['mse']
+)
+
+start = time.time()
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=10,
+    verbose=2,
+    restore_best_weights=True
+)
+
+model.fit(
+    x_train, y_train,
+    epochs=1000,
+    batch_size=32,
+    validation_split=0.2,
+    callbacks=[early_stopping]
+)
+
+end = time.time()
+
+
+# 4. 평가, 예측
+
+loss = model.evaluate(x_test, y_test)
+y_predict = model.predict(x_test)
+
+
+def RMSE(y_test, y_predict):
+    return(np.sqrt(mean_squared_error(y_test, y_predict)))
+
+
+print('loss:', loss)
+print('RMSE:', RMSE(y_test, y_predict))
+print('r2:', r2_score(y_test, y_predict))
+print('걸린시간 : ', end-start)
